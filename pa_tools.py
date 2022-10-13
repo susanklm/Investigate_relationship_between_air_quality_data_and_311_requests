ef70cc6b8682c888b77b982cbc6a40a5d79c1286
Susan K <susankol@alumni.usc.edu>
2022-10-13 12:56:16 -0700

    add some descriptions

--- pa_tools.py
+++ pa_tools.py
@@ -5,6 +5,7 @@ import pandas as pd
 import time
 import datetime
 import json
+import sys
 from io import StringIO
 from sqlalchemy import create_engine
 import os
@@ -51,6 +52,7 @@ def get_sensorslist(nwlng,nwlat,selng,selat,location,key_read, fname):
             
     # Final API URL
     api_url = root_url + f'?api_key={key_read}' + fields_api_url + ll_api_url + loc_api
+    #print(api_url)
 
     # Getting data
     response = requests.get(api_url)
@@ -58,8 +60,11 @@ def get_sensorslist(nwlng,nwlat,selng,selat,location,key_read, fname):
     if response.status_code == 200:
         #print(response.text)
         json_data = json.loads(response.content)['data']
-        df = pd.DataFrame.from_records(json_data)
-        df.columns = fields_list
+        if len(json_data) == 0:
+            df = pd.DataFrame(columns = fields_list)
+        elif len(json_data) != 0:
+            df = pd.DataFrame.from_records(json_data)
+            df.columns = fields_list
     else:
         print("Error")
         #raise requests.exceptions.RequestException
@@ -119,21 +124,29 @@ def get_sensors_df(nwlng,nwlat,selng,selat,location,key_read):
     if response.status_code == 200:
         #print(response.text)
         json_data = json.loads(response.content)['data']
-        df = pd.DataFrame.from_records(json_data)
-        df.columns = fields_list
+        if len(json_data) == 0:
+            df = pd.DataFrame(columns = fields_list)
+        elif len(json_data) != 0:
+            df = pd.DataFrame.from_records(json_data)
+            df.columns = fields_list
     else:
         print("Error")
         #raise requests.exceptions.RequestException
     
     return df
 
-# Function to plot sensors on a folium map from a sensors_list
+# Function which input a dataframe of sensor list to plot sensors' location on a folium map 
 def plot_sensors(sensors_list):
     latitude = (sensors_list['latitude'].max()+sensors_list['latitude'].min())/2
     longitude = (sensors_list['longitude'].max()+sensors_list['longitude'].min())/2
     
     nc_geo = r'Neighborhood_Councils_(Certified).geojson'
     m5 = folium.Map(location=[latitude, longitude], tiles = 'CartoDB positron', zoom_start=11, min_zoom=8,max_zoom=15)
+    
+    # Add several TileLayers
+    folium.TileLayer('openstreetmap').add_to(m5)
+    folium.TileLayer('stamenterrain').add_to(m5)
+    
     cpleth = folium.Choropleth(geo_data = nc_geo,
             name='Choropleth',
             key_on='features.properties.Name',
@@ -147,7 +160,39 @@ def plot_sensors(sensors_list):
                 tooltip=tooltip_text).add_to(m5)
     return m5
 
-# Function which input sensor_index and read_key and output sensor name, index, location_type, latitude, and longitude
+# Function which input a dataframe of sensor list to plot the location of sensors with circle surround each sensor on a folium map
+def plot_sensors_with_circle(sensors_list, radius):
+    latitude = (sensors_list['latitude'].max()+sensors_list['latitude'].min())/2
+    longitude = (sensors_list['longitude'].max()+sensors_list['longitude'].min())/2
+    
+    nc_geo = r'Neighborhood_Councils_(Certified).geojson'
+    m5 = folium.Map(location=[latitude, longitude], tiles = 'CartoDB positron', zoom_start=11, min_zoom=8,max_zoom=15)
+    
+    # Add several TileLayers
+    folium.TileLayer('openstreetmap').add_to(m5)
+    folium.TileLayer('stamenterrain').add_to(m5)
+    
+    cpleth = folium.Choropleth(geo_data = nc_geo,
+            name='Choropleth',
+            key_on='features.properties.Name',
+            fill_opacity=0.1, line_opacity=0.3
+            ).add_to(m5) 
+    folium.LayerControl().add_to(m5)
+    
+    for i, lat, lng, sensor_name, sensor_index in zip(sensors_list.index.values.tolist(),sensors_list.latitude, sensors_list.longitude, sensors_list.name, sensors_list.sensor_index):
+        tooltip_text = sensor_name+", ID: " + str(sensor_index)
+        folium.CircleMarker([lat, lng], radius = 15, color = "green", weight=1,
+                fill = True, fill_color = "green", fill_opacity = 0.1,
+                tooltip=tooltip_text).add_to(m5)
+        folium.Circle([lat, lng], radius = radius, color = "blue",  weight=1.5,
+                             ).add_to(m5)
+        folium.Marker([lat, lng],
+        icon=folium.DivIcon(html=f"""<div style="font-family: Arial; 
+        color: b; font-weight: bold; font size="+5"">{i}</div>""")
+        ).add_to(m5)
+    return m5
+
+# Function which input sensor_index and read_key and return sensor name, index, location_type, latitude, and longitude
 def create_s_list(sensor_index, READ_KEY):
     root_url = 'https://api.purpleair.com/v1/sensors/' + str(sensor_index)
     api_url = root_url + f'?api_key={READ_KEY}' 
@@ -155,9 +200,9 @@ def create_s_list(sensor_index, READ_KEY):
     s_list = [results['sensor']['name'], results['sensor']['sensor_index'], results['sensor']['location_type'], results['sensor']['latitude'], results['sensor']['longitude']]
     return s_list
 
-###############################################################################################################
-# Below, whithin the "###" closure is a set of functions to calculate AQI for PM2.5 and PM10.0
-
+# Below, whithin the "###" enclosure is a set of functions to calculate AQI for PM2.5 and PM10.0
+#################################################################################################################################
+# Function to calculate AQI in general
 def calcAQI(Cp, Ih, Il, BPh, BPl):
     a = (Ih - Il)
     b = (BPh - BPl)
@@ -175,7 +220,7 @@ def calcAQI(Cp, Ih, Il, BPh, BPl):
 #    Hazardous                        401 – 500  |  350.5 – 500.4
 #    Hazardous                        501 – 999  |  505.5 – 99999.9
         
-# Calculate the AQI of pm2.5_cf_1
+# Function to calculate the AQI of pm2.5_cf_1. It input the pm2.5 concentration and return pm2.5 AQI
 def aqiFrom25PM(pm):
     if not pm:
         return "-"
@@ -215,7 +260,7 @@ def aqiFrom25PM(pm):
 #    Hazardous                        401 – 500  |  505.0 – 604.0
 #    Hazardous                        501 – 999  |  605.0 – 99999.0
     
-# Calculate the AQI of pm10.1_cf_1
+# Function to calculate the AQI of pm10.1_cf_1. It input the pm10.0 concentration and return pm10.0 AQI
 def aqiFrom10PM(pm):
     if not pm:
         return "-"
@@ -242,9 +287,9 @@ def aqiFrom10PM(pm):
         return calcAQI(pm, 50, 0, 54.0, 0.0) #Good
     else: 
         return None
-###############################################################################################################
+##############################################################################################################################
 
-# Create a Dataframe from the API response and return a filename of the created dataframe. It input the response from the API,
+# Function to create a dataframe from the API response and return a filename of the created dataframe. It input the response from the API,
 # sensor_index, and a dataframe of 10 selected sensors.
 def create_df(response, sensor_index, sensors_loc_df):
     column_names = response['fields']
@@ -284,7 +329,7 @@ def create_df(response, sensor_index, sensors_loc_df):
     temp_df.to_csv(filename, index=False, header=True)
     return filename
 
-# Create a Dataframe of 311 Requests surrounding the selected 10 sensors and return a filename of the created dataframe. It input a dataframe of 311 requests, selected 10 sensor locations, and desired filename
+# Function to create a dataframe of 311 Requests surrounding the selected 10 sensors and return a filename of the created dataframe. It input a dataframe of 311 requests, selected 10 sensor locations, and desired filename
 def create_request_sensor_df(df21, sensors_loc_df, fname):
     # Create a dataframe which consists of 311 Requests/Services surrounding the 10 Selected Sensors
     column_names = ['CreatedDate', 'RequestType', 'ServiceDate', 'Latitude', 'Longitude', 'DistToSensor', 'SensorName', 'SensorIndex', 'Sensor#']
@@ -308,3 +353,76 @@ def create_request_sensor_df(df21, sensors_loc_df, fname):
     filename = os.path.join(folderpath, fname + suffix)
     temp_df.to_csv(filename, index=False)
     return filename
+
+
+# Function to find sensor with complete timestamps (from unix_start_time, unix_end_time). 
+# It input sensors_list_all which is obtained from get_sensors_df, unix_start_time, unix_end_time, and API header.
+# And it return a list of sensor where sensor_index, name, location_type, latitude, longitude
+def find_sensor_w_complete_row(sensors_list_all, unix_start_time, unix_end_time, headers):
+    average = 1440
+    sensors_list_new = []
+    fields_list = ['sensor_index','name','location_type', 'latitude','longitude'] 
+
+    for idx, name, loc_type, lat, lon in zip(sensors_list_all.sensor_index, sensors_list_all.name, sensors_list_all.location_type, sensors_list_all.latitude, sensors_list_all.longitude):
+        response = requests.get(f"https://api.purpleair.com/v1/sensors/{idx}/history?start_timestamp={unix_start_time}&end_timestamp={unix_end_time}&average={average}&fields=pm2.5_cf_1%2C%20pm10.0_cf_1", headers=headers).json()
+    
+        column_names = response['fields']
+        temp_df = pd.DataFrame(response['data'], columns = column_names)
+        temp_date = []
+    
+        for i in range(len(temp_df['time_stamp'])):
+            date_time = datetime.datetime.fromtimestamp(temp_df['time_stamp'][i])
+            temp_date.append(date_time.strftime('%Y-%m-%d'))
+    
+        temp_df['time_stamp'] = pd.to_datetime(temp_date)
+
+        # Setting the Date values as index
+        temp_index = temp_df.set_index('time_stamp')
+
+        # dates which are not in the sequence are returned
+        miss_date = pd.date_range(start="2021-01-01", end="2021-12-31").difference(temp_index.index)
+
+        if ((len(miss_date) == 0) and (temp_df["pm2.5_cf_1"].isnull().sum() == 0) and (temp_df["pm10.0_cf_1"].isnull().sum() == 0)):
+            fields = [idx, name, loc_type, lat, lon]
+            sensors_list_new.append(fields)
+    new_df = pd.DataFrame(sensors_list_new, columns = fields_list)
+    return new_df
+
+# Function to fine a sample of semsors. It input sensors dataframe, sample size, and radius_in_miles and return a dataframe of sample
+def get_one_sample(df, sample_size, radius_in_miles):
+    all_sensors = df.copy()
+    random_sensor1 = all_sensors.sample(n = 1)
+    selected_sensors = random_sensor1.values.tolist()
+    all_sensors.drop(all_sensors[all_sensors['sensor_index'] == random_sensor1["sensor_index"].values[0]].index, inplace = True)
+    
+    while len(selected_sensors) <= sample_size:
+        random_sensor2a = all_sensors.sample(n=1)
+        random_sensor2b = random_sensor2a.values.tolist()
+
+        sensor_loc2 = [random_sensor2b[0][3], random_sensor2b[0][4]]
+        fields = [random_sensor2b[0][0], random_sensor2b[0][1], random_sensor2b[0][2], random_sensor2b[0][3], random_sensor2b[0][4]]
+        all_sensors.drop(all_sensors[all_sensors['sensor_index'] == random_sensor2a["sensor_index"].values[0]].index, inplace = True)
+        #print("length all_sensors: ",len(all_sensors))
+        distance_list = []
+    
+        for i in range(len(selected_sensors)):
+            sensor_loc1 = [selected_sensors[i][3], selected_sensors[i][4]]
+            #print("selected_sensor", selected_sensors[i][1])
+            #print("random_sensor2", random_sensor2b[0][1])
+        
+            d = gd(sensor_loc2, sensor_loc1).miles
+        
+            distance_list.append(d)
+            #print(distance_list)
+    
+        diameter = 2*radius_in_miles
+        if all([dis >= diameter for dis in distance_list]):
+            if fields not in selected_sensors:
+                selected_sensors.append(fields)     
+            #print('Added sensor', random_sensor2b[0][1])
+            #print(len(selected_sensors))
+            #print("") 
+    
+    column_names = ['sensor_index', 'name', 'location_type', 'latitude', 'longitude']
+    temp_df = pd.DataFrame(selected_sensors, columns = column_names)
+    return temp_df
